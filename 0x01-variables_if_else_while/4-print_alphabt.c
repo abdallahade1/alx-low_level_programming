7c5a4f71a6540122225670b9932022df38cd4310
abdallahade1 <abdoadelshabaan@gmail.com>
2023-07-15 14:23:52 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <sdio.h>
+
+/**
+ * main - Entry
+ *
+ * Description: Print all alpha except q amd e
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+			ch++;
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
