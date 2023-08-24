a36a674cb69377c6a58f176fa8a82da2f8569a3b
abdallahade1 <abdoadelshabaan@gmail.com>
2023-08-24 14:27:08 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
  * list_len - measure the lenght of the linked list
- * @h: the pointer to the first node
+ * @h: pointer to the first node
  *
- * Return: Size of the list
+ * Return: lenght of the list
  */
+
 size_t list_len(const list_t *h);
 {
 	size_t i = 0;
