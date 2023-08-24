1f4ed2c3cf1405307d1f62c020e05e45e5606b0a
abdallahade1 <abdoadelshabaan@gmail.com>
2023-08-24 15:03:08 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,17 +14,10 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (!new_node)
 		return (NULL);
-	if (str)
-	{
+
 		new_node->str = strdup(str);
-		if (!new_node->str)
-		{
-			free(new_node);
-			return (NULL);
-		}
 		new_node->len = _strlen(new_node->str);
-	}
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 }
