6dbf2edadf8fb479be707d349a6a2533e612d730
abdallahade1 <abdoadelshabaan@gmail.com>
2023-08-24 14:17:38 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - measure the lenght of the linked list
+ * @h: the pointer to the first node
+ *
+ * Return: Size of the list
+ */
+size_t list_len(const list_t *h);
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
