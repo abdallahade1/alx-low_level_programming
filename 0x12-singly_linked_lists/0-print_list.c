d577aa898b925394141b0b71b9c4000d9deb8c98
abdallahade1 <abdoadelshabaan@gmail.com>
2023-08-24 13:29:08 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 
 /**
+ * _strlen - returns the length of the string
+ * @s: The sring I used to check the length
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int j = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		j++;
+	return (j);
+}
+
+/**
  * print_list - prints the linked list
  * @h: points to the first node in the list
  * Return: The number of nodes
@@ -8,14 +25,14 @@
 
 size_t print_list(const list_t *h);
 {
-	size_t i = 0;
+	size_t s = 0;
 
 	while (h)
 	{
-		if (h->NULL)
-			printf("[0] (nil)\n");
+		if (!h->str)
+			printf("(nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%d] %s\n", _strlen(h->str), h->str);
 		h = h->next;
 		i++;
 	}
