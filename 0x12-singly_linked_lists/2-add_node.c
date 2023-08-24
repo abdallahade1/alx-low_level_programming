d9a6242b3edf4c5a521fb62bf4da4bdf8edd7a20
abdallahade1 <abdoadelshabaan@gmail.com>
2023-08-24 14:55:01 -0700

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the biggining of the list
+ * @head: points to the head node
+ * @str: data field of the node
+ *
+ * Return: size of the length
+ */
+
+list_t *add_node(list_t **head, const char *str);
+{
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (!new_node)
+		return (NULL);
+	if (str)
+	{
+		new_node->str = strdup(str);
+		if (!new_node->str)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		new_node->len = _strlen(new_node->str);
+	}
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
