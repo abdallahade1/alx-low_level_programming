5cc92625eec32c72238ec87bc955640c46deb16f
abdallahade1 <abdoadelshabaan@gmail.com>
2023-08-29 06:59:20 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,16 +8,16 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *node;
+	listint_t *node, *temp;
 
 	if (head == NULL)
 		return;
-
-	while (head)
+	node = *head;
+	while (node)
 	{
-		node = head;
-		head = head->next;
-		free(node);
+		temp = node;
+		node = node->next;
+		free(temp);
 	}
 
 	*head = NULL;
