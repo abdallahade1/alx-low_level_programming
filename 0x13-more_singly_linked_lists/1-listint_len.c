39eb9893359ffb1ac2fe95c748a470c690bb883e
abdallahade1 <abdoadelshabaan@gmail.com>
2023-08-29 05:30:18 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,4 +2,19 @@
 
 /**
  * listint_len - prints the number of elements in the linked list
- * @h: 
+ * @h: points at the linked list
+ * Return: num of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t j = 0;
+
+	while (h)
+	{
+		j++;
+		h = h->next;
+	}
+
+	return (j);
+}
