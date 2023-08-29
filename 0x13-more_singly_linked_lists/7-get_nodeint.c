924dde548384850e8036b252ceed008ae4a066b6
abdallahade1 <abdoadelshabaan@gmail.com>
2023-08-29 07:22:12 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,10 +11,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *node = head;
 
-	while (node)
+	while (node && i < index)
 	{
 		node = node->next;
 		i++;
 	}
-	return (node ? node : NULL)
+	return (node ? node : NULL);
 }
