a7096ff6ffb3b24ec49d252d48dacd598786c338
abdallahade1 <abdoadelshabaan@gmail.com>
2023-09-04 07:18:32 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	if (!text_content)
+	while (text_content)
 	{
 		for (l = 0; text_content[l];)
 			l++;
