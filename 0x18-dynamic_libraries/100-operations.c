a4e2953230d1db0cc9acd64bd1104d27678c4d12
abdallahade1 <abdoadelshabaan@gmail.com>
2023-10-16 14:02:10 -0700

    task2

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+/**
+ * _add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of a and b.
+ */
+int _add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * subtract - Subtracts two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of a - b.
+ */
+int subtract(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * multiply - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The product of a and b.
+ */
+int multiply(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * divide - Divides two integers.
+ * @a: The dividend.
+ * @b: The divisor.
+ *
+ * Return: The result of a / b. If b is 0, returns 0 with an error message.
+ */
+
+int divide(int a, int b)
+{
+	if (b == 0)
+	{
+	    printf("Error: Division by zero\n");
+	return (0);
+	}
+	return (a / b);
+}
+
+/**
+ * modulus - Calculates the modulus of two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of a % b. If b is 0, returns 0 with an error message.
+ */
+int modulus(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error: Modulus by zero\n");
+	return (0);
+	}
+	return (a % b);
+
+}
