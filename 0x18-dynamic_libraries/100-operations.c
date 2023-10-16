633b9d232664b8a06c5e895de8cb0e0fd52942e8
abdallahade1 <abdoadelshabaan@gmail.com>
2023-10-16 14:38:33 -0700

    100-operations.c

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -13,38 +13,38 @@ int add(int a, int b)
 }
 
 /**
- * subtract - Subtracts two integers.
+ * sub - Subtracts two integers.
  * @a: The first integer.
  * @b: The second integer.
  *
  * Return: The result of a - b.
  */
-int subtract(int a, int b)
+int sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * multiply - Multiplies two integers.
+ * mul - Multiplies two integers.
  * @a: The first integer.
  * @b: The second integer.
  *
  * Return: The product of a and b.
  */
-int multiply(int a, int b)
+int mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * divide - Divides two integers.
+ * div - Divides two integers.
  * @a: The dividend.
  * @b: The divisor.
  *
  * Return: The result of a / b. If b is 0, returns 0 with an error message.
  */
 
-int divide(int a, int b)
+int div(int a, int b)
 {
 	if (b == 0)
 	{
@@ -55,13 +55,13 @@ int divide(int a, int b)
 }
 
 /**
- * modulus - Calculates the modulus of two integers.
+ * mod - Calculates the modulus of two integers.
  * @a: The first integer.
  * @b: The second integer.
  *
  * Return: The result of a % b. If b is 0, returns 0 with an error message.
  */
-int modulus(int a, int b)
+int mod(int a, int b)
 {
 	if (b == 0)
 	{
