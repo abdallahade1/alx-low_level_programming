3fc8234f36ba7f2234690493af24915593e50c8b
abdallahade1 <abdoadelshabaan@gmail.com>
2023-10-16 14:32:39 -0700

    test2

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * _add - Adds two integers.
+ * add - Adds two integers.
  * @a: The first integer.
  * @b: The second integer.
  *
  * Return: The sum of a and b.
  */
-int _add(int a, int b)
+int add(int a, int b)
 {
 	return (a + b);
 }
