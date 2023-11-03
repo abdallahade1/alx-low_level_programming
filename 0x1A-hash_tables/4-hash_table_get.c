308295308f5ad6351cc59ba6ee1cf93273e3a928
abdallahade1 <abdoadelshabaan@gmail.com>
2023-11-03 14:52:18 -0700

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -4,9 +4,9 @@
  * hash_table_get - function to retrive value
  * @ht: pointer to hash table
  * @key: the key to get the value from
+ *
  * Return: value or NULL if key not found
  */
-
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *current_node;
@@ -20,7 +20,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 
 	current_node = ht->array[idx];
-	while (current_node && strcmp(currentnode->key, key) != 0)
+	while (current_node && strcmp(current_node->key, key) != 0)
 		current_node = current_node->next;
 
 	return ((current_node == NULL) ? NULL : current_node->value);
